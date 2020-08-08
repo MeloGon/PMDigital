109150496f9f445a34c930948bfc0250a6ecac2e
Kevyn Melo <troy.esname@gmail.com>
2020-08-08 09:45:15 -0500

    Vista actualizada de Menu

--- lib/src/pages/menu_page.dart
+++ lib/src/pages/menu_page.dart
@@ -11,6 +11,7 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   Color _colorGradbeg = Color(0xffDFE3E4);
   Color _colorGradend = Color(0xffF3F4F5);
+  TextStyle _titleStyle = TextStyle(fontSize: 24.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,43 +20,16 @@ class _MenuPageState extends State<MenuPage> {
         _loguinBg(),
         SingleChildScrollView(
           child: Column(
-            children: <Widget>[_titulos(), _botonesRedondeados()],
+            children: <Widget>[
+              _titulos("Ordenes de Trabajo"),
+              _botonesRedondeados(),
+              _titulos("Activos"),
+              _botonesRedondeados1()
+            ],
           ),
         )
       ],
     ));
-  }
-
-  Widget _fondoApp() {
-    final gradiente = Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: FractionalOffset(0.0, 0.6),
-              end: FractionalOffset(0.0, 1.0),
-              colors: [
-            Color.fromRGBO(52, 54, 101, 1.0),
-            Color.fromRGBO(35, 37, 57, 1.0)
-          ])),
-    );
-
-    final cajaRosa = Transform.rotate(
-        angle: -pi / 5.0,
-        child: Container(
-          height: 360.0,
-          width: 360.0,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(80.0),
-              gradient: LinearGradient(colors: [
-                Color.fromRGBO(236, 98, 188, 1.0),
-                Color.fromRGBO(241, 142, 172, 1.0)
-              ])),
-        ));
-
-    return Stack(
-      children: <Widget>[gradiente, Positioned(top: -100.0, child: cajaRosa)],
-    );
   }
 
   Widget _loguinBg() {
@@ -71,15 +45,15 @@ class _MenuPageState extends State<MenuPage> {
     );
   }
 
-  Widget _titulos() {
+  Widget _titulos(String titleValue) {
     return SafeArea(
       child: Container(
-        padding: EdgeInsets.all(20.0),
+        width: double.infinity,
+        padding: EdgeInsets.only(top: 20.0, left: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Ordenes de Trabajo',
-                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+            Text(titleValue.toString(), style: _titleStyle),
           ],
         ),
       ),
@@ -94,10 +68,26 @@ class _MenuPageState extends State<MenuPage> {
           _crearBotonRedondeado(
               Colors.purpleAccent, Icons.directions_bus, 'Bus'),
         ]),
+        // TableRow(children: [
+        //   _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
+        //   _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'File'),
+        // ]),
+      ],
+    );
+  }
+
+  Widget _botonesRedondeados1() {
+    return Table(
+      children: [
         TableRow(children: [
-          _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
-          _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'File'),
+          _crearBotonRedondeado(Colors.blue, Icons.border_all, 'General'),
+          _crearBotonRedondeado(
+              Colors.purpleAccent, Icons.directions_bus, 'Bus'),
         ]),
+        // TableRow(children: [
+        //   _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
+        //   _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'File'),
+        // ]),
       ],
     );
   }
